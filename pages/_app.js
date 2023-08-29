5a91fc109d2772654239df67d871b6b7830ee56b
Georg Mavr <g.mavraganis@yahoo.com>
2023-08-29 15:23:53 +0200

    json/fetch/HomePage

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { data } = useSWR("/records.json", fetcher);
+  if (!data) return;
+  console.log(data);
+
+  return <Component {...pageProps} data={data} />;
 }
