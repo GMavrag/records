b66ca5579c208f74229ac2404c9cdfad58833de3
Georg Mavr <g.mavraganis@yahoo.com>
2023-09-12 14:18:12 +0200

    delete right,homelink

--- pages/add-record.js
+++ pages/add-record.js
@@ -1,15 +1,16 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 import RecordForm from "../components/RecordForm";
 
 export default function AddRecord() {
   const { data: session } = useSession();
-
   if (session) {
     return (
       <div>
         <h1>Add New Record</h1>
         <RecordForm />
+        <Link href="/">Home</Link>
       </div>
     );
   }
